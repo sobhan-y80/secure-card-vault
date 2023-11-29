15d1dc964cd0d9851e17d96c6b94638fec851106
unknown <sobhan13831383yaghobi@gmail.com>
2023-11-29 17:42:16 +0330

    connect card lists to localstorage and handel logic of add form finish

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,7 @@ const clearForm = () => {
 };
 const saveCardAction = () => {
     const newInfo = {
+        id: crypto.randomUUID(),
         ownerName: ownerCardInputElm.value.trim(),
         bankName: bankNameInputElm.value.trim(),
         cardBank: getCardNumber().trim(),
@@ -42,6 +43,54 @@ const saveCardAction = () => {
     const mainCards = Array.isArray(cards) ? [...cards, { ...newInfo }] : [{ ...newInfo }];
     saveLocalStorage("cards", mainCards);
     clearForm();
+    renderCard();
+};
+
+const renderCard = () => {
+    const cardsWrapperElm = document.querySelector(".card__wrapper");
+    const cardList = getLocalStorage("cards");
+    if (Array.isArray(cardList)) {
+        cardsWrapperElm.classList.remove("hidden");
+        const cardListElm = cardList.map(
+            (card) =>
+                `<div id="${card.id}" class="card">
+                    <button class="card__remove_btn">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 5L19 19M5.00003 19L12 12L19 5" stroke="#2D264B" stroke-width="1.5" stroke-linecap="round"/>
+                        </svg>
+                    </button>
+                    <div class="circle_card"></div>
+                    <h1 class="card__bank_name">${card?.bankName}</h1>
+                    <div class="card__chip_wrapper">
+                        <img class="card__chip" src="/public/images/credit-chip.png" alt="" />
+                    </div>
+                    <div title="click to copy">
+                        <div id="card__card_number_wrapper" class="pointer">
+                        ${card?.cardBank
+                            .match(/.{1,4}/g)
+                            .map((number) => `<span>${number}</span>`)
+                            .join(" ")}
+                        </div>
+                        <div class="card__date">
+                            <span id="year" class="pointer" title="click to copy">
+                                ${card?.year}
+                            </span>
+                            /
+                            <span id="month" class="pointer">
+                                ${card?.month}
+                            </span>
+                        </div>
+                        <div id="cvv2" class="pointer">CVV2 : ${card?.cvv2}</div>
+                        <div title="click to copy" id="card__owner_name" class="pointer">
+                            ${card?.ownerName}
+                        </div>
+                    </div>
+                </div>`
+        );
+        cardsWrapperElm.innerHTML = `<h2>Card List</h2> ${cardListElm.join(" ")}`;
+    } else {
+        cardsWrapperElm.classList.add("hidden");
+    }
 };
 
 //$$ Evnet Handler
@@ -66,3 +115,5 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     saveCardAction();
 });
+
+window.addEventListener("load", renderCard);
