200a47f25e8a15b4bd34b4cbcd0724a91789f4fb
unknown <sobhan13831383yaghobi@gmail.com>
2023-11-29 08:08:21 +0330

    handel card number input field logic finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-// js
+//$$ Variables
+const cardNumberInputsElm = document.querySelectorAll(".form__card_number_field");
+
+//$$ Methods
+
+const isClassList = (mainClassList, className) => mainClassList?.contains(className);
+
+//$$ Evnet Handler
+cardNumberInputsElm.forEach((inputFiled) => {
+    inputFiled.addEventListener("focus", () => inputFiled.select());
+    inputFiled.addEventListener("input", (e) => {
+        if (e.target.value.length === 4) {
+            isClassList(e.target.nextElementSibling?.classList, "form__card_number_field")
+                ? e.target.nextElementSibling.focus()
+                : undefined;
+        } else if (e.target.value.length === 0) {
+            isClassList(e.target.previousElementSibling?.classList, "form__card_number_field")
+                ? e.target.previousElementSibling.focus()
+                : undefined;
+        }
+    });
+});
